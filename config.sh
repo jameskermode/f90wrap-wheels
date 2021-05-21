db09d9e267dbb4600b6076ba329464acfba9b76a
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-05-21 11:45:34 +0100

    simplify testing

--- config.sh
+++ config.sh
@@ -4,7 +4,6 @@
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pip download f90wrap==${F90WRAP_VERSION} --no-deps --no-binary :all:
-    tar xzf f90wrap-${F90WRAP_VERSION}.tar.gz
-    (cd f90wrap-${F90WRAP_VERSION}/examples; PYTHON=python make test)
+    f90wrap --help
+    f2py-f90wrap --help
 }
