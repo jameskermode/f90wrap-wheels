1074e4fabaf196bd40b3c6cd9b89dcfc89b34daa
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-05-25 08:57:20 +0100

    set F90FLAGS for pip wheel build

--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
 # Define custom utilities
 # Test for macOS with [ -n "$IS_MACOS" ]
 
+export F90FLAGS=-fPIC # ensure Fortran sources are compiled by numpy setuptools as position independent code
+
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
