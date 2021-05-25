2284217d80e35a7e9a592989c8bfd91f418895ed
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-05-25 12:43:50 +0100

    try to run tests

--- config.sh
+++ config.sh
@@ -3,9 +3,25 @@
 
 export F90FLAGS=-fPIC # ensure Fortran sources are compiled by numpy setuptools as position independent code
 
-function run_tests {
-    # Runs tests on installed distribution from an empty directory
-    python --version
-    f90wrap --help
-    f2py-f90wrap -h
+function run_rests{
+    F90WRAP_VERSION=$1
+
+    # clone repo, checkout release branch and run tests
+    apt-get install -y git
+    git clone --depth 1 https://github.com/jameskermode/f90wrap
+    cd f90wrap
+    git checkout v${F90WRAP_VERSION}
+    cd examples
+    python3 run_all.py -v
+
+}
+
+function install_run{
+    PLAT=$1
+    F90WRAP_VERSION=$2
+
+    install_wheel
+    mkdir tmp_for_test
+    (cd tmp_for_test && run_tests ${F90WRAP_VERSION})
+    rmdir tmp_for_test  2>/dev/null || echo "Cannot remove tmp_for_test"
 }
