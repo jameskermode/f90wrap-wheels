7d82db8932751adb121ea6f4eac763c9d0398b9f
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-06-19 19:36:03 +0100

    build from submodule not pip

--- config.sh
+++ config.sh
@@ -3,8 +3,6 @@
 
 export F90FLAGS=-fPIC # ensure Fortran sources are compiled by numpy setuptools as position independent code
 
-export F90WRAP_VERSION=0.2.6 # FIXME should avoid duplicating this from .travis.yml
-
 source gfortran-install/gfortran_utils.sh
 
 function pre_build {
@@ -13,17 +11,9 @@ function pre_build {
         # ensure we use the cross-compiler for Fortran 90 as well F77
         export F90=/opt/gfortran-darwin-arm64/bin/arm64-apple-darwin20.0.0-gfortran
     fi
-    which python
-    python -c "import sys; print(sys.version)"
 }
 
 function run_tests {
-    # clone repo, checkout release branch and run tests
-    if [[ ! -n "$IS_MACOS" ]]; then
-        apt-get install -y git
-        apt-get install -y gfortran
-    fi
-    git clone --depth 1 --branch v${F90WRAP_VERSION} https://github.com/jameskermode/f90wrap
     cd f90wrap
     cd examples
     make test
